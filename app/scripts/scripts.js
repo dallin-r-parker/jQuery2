4da6b5fa4aca5c893442da1f29a6c67834f0be9c
dallin-r-parker <dallin.r.parker@gmail.com>
2016-09-21 22:15:27 -0600

    started

---
+++ app/scripts/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('logo-container').mouseenter(function () {
+        $('.logo-container').fadeTo('fast', 1);
+    });
+});
+
+$(document).ready(function () {
+    $("<p>I'm a paragraph!</p>").append('body');
+});
+git
