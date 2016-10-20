ebad9484f6534e4cc28a323111f9b071a6141ff0
dallin-r-parker <dallin.r.parker@gmail.com>
2016-10-20 16:18:18 -0600

    new files

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -90,4 +90,31 @@ $(document).ready(function () {
         advanceTask(task);
     });
 
-}); //<--- this is the end of document.ready
+    <!doctype html>
+    <html lang="en">
+        <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport"
+    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        //Add CSS Links Here
+        <title>Document</title>
+        </head>
+        <body>
+
+
+
+
+
+
+
+
+
+
+
+        <!--jQuery-->
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <!--INclude your script file here -->
+    </body>
+    </html> //<--- this is the end of document.ready
